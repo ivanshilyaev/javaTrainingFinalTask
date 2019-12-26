c7a9d7183fac8994de29682cea58d9066f84c25b
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-12-26 12:07:41 +0300

    Method added to print a list of trains with common seats, going to a given destination

--- task08/src/main/java/by/training/oop/bookassignment10/controller/Runner.java
+++ task08/src/main/java/by/training/oop/bookassignment10/controller/Runner.java
@@ -1,5 +1,6 @@
 package by.training.oop.bookassignment10.controller;
 
+import by.training.oop.bookassignment10.bean.Seat;
 import by.training.oop.bookassignment10.bean.Train;
 import by.training.oop.bookassignment10.service.TrainListCommand;
 import by.training.oop.bookassignment10.service.creator.TrainCreator;
@@ -20,25 +21,39 @@ public class Runner {
             consoleHelper.printTrains(trains, "Train array:");
 
             // #1 list of trains, going to a given destination
-            String destination = consoleHelper.readLine("1. Enter a destination:");
-            Train[] list1 = command.getListOfSuitableTrains(trains, destination);
-            consoleHelper.printTrains(list1, "List of trains, going to a given destination:");
+            try {
+                String destination = consoleHelper.readLine("1. Enter a destination:");
+                Train[] list1 = command.getListOfSuitableTrains(trains, destination);
+                consoleHelper.printTrains(list1, "List of trains, going to a given destination:");
+            } catch (ServiceException e) {
+                consoleHelper.printString(e.getMessage());
+            }
 
             // #2 list of trains, going to a given destination
             // and living after a given day (date)
-            destination = consoleHelper.readLine("2. Enter a destination:");
-            Date date = consoleHelper.readDate("Enter date in format dd.mm.yyyy HH:mm" +
-                    " after which you'd line to search the train");
-            Train[] list2 = command.getListOfSuitableTrains(trains, destination, date);
-            consoleHelper.printTrains(list2, "List of trains, going to a given destination" +
-                    " and living after a given day:");
+            try {
+                String destination = consoleHelper.readLine("2. Enter a destination:");
+                Date date = consoleHelper.readDate("Enter date in format dd.mm.yyyy HH:mm" +
+                        " after which you'd line to search the train");
+                Train[] list2 = command.getListOfSuitableTrains(trains, destination, date);
+                consoleHelper.printTrains(list2, "List of trains, going to a given destination" +
+                        " and living after a given day:");
+            } catch (ServiceException e) {
+                consoleHelper.printString(e.getMessage());
+            } catch (InvalidParameterException e) {
+                consoleHelper.printString(e.getMessage());
+            }
 
             // #3 list of trains with common seats, going to a given destination
+            try {
+                String destination = consoleHelper.readLine("1. Enter a destination:");
+                Train[] list3 = command.getListOfSuitableTrains(trains, destination, Seat.COMMON);
+                consoleHelper.printTrains(list3, "List of trains with common seats, " +
+                        "going to a given destination:");
+            } catch (ServiceException e) {
+                consoleHelper.printString(e.getMessage());
+            }
         } catch (CreationException e) {
-            consoleHelper.printString(e.getMessage());
-        } catch (ServiceException e) {
-            consoleHelper.printString(e.getMessage());
-        } catch (InvalidParameterException e) {
             consoleHelper.printString(e.getMessage());
         }
         // each exception could be processed differently
--- task08/src/main/java/by/training/oop/bookassignment10/service/TrainListCommand.java
+++ task08/src/main/java/by/training/oop/bookassignment10/service/TrainListCommand.java
@@ -1,11 +1,14 @@
 package by.training.oop.bookassignment10.service;
 
+import by.training.oop.bookassignment10.bean.Seat;
 import by.training.oop.bookassignment10.bean.Train;
 import by.training.oop.bookassignment10.service.exception.ServiceException;
 
 import java.util.Date;
 
 public class TrainListCommand {
+    private static final String ERROR_MESSAGE = "No suitable trains!";
+
     public Train[] getListOfSuitableTrains(Train[] trains, String destination)
             throws ServiceException {
         int count = 0;
@@ -15,7 +18,7 @@ public class TrainListCommand {
             }
         }
         if (count == 0) {
-            throw new ServiceException("No suitable trains!");
+            throw new ServiceException(ERROR_MESSAGE);
         }
         Train[] result = new Train[count];
         int j = 0;
@@ -37,13 +40,36 @@ public class TrainListCommand {
             }
         }
         if (count == 0) {
-            throw new ServiceException("No suitable trains!");
+            throw new ServiceException(ERROR_MESSAGE);
         }
         Train[] result = new Train[count];
         int j = 0;
         for (Train train : trains) {
             if (train.getDestination().equalsIgnoreCase(destination)
                     && train.getDepartureTime().compareTo(date) < 0) {
+                result[j++] = train;
+            }
+        }
+        return result;
+    }
+
+    public Train[] getListOfSuitableTrains(Train[] trains, String destination,
+                                           Seat seat) throws ServiceException {
+        int count = 0;
+        for (Train train : trains) {
+            if (train.getDestination().equalsIgnoreCase(destination)
+                    && train.getNumberOfSeats().get(seat) > 0) {
+                ++count;
+            }
+        }
+        if (count == 0) {
+            throw new ServiceException(ERROR_MESSAGE);
+        }
+        Train[] result = new Train[count];
+        int j = 0;
+        for (Train train : trains) {
+            if (train.getDestination().equalsIgnoreCase(destination)
+                    && train.getNumberOfSeats().get(seat) > 0) {
                 result[j++] = train;
             }
         }
