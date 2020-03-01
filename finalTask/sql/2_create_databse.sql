27c74f2779445b357aa237fe06ca475d508d71eb
Ivan Shilyaev <shilyaev1433@gmail.com>
2020-03-01 12:33:46 +0300

    Final task: tables created

--- finalTask/sql/2_create_databse.sql
+++ finalTask/sql/2_create_databse.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE `account_db` DEFAULT CHARACTER SET utf8;
-CREATE USER `application` on "some_password"
+CREATE DATABASE account_db;
+CREATE USER application IDENTIFIED BY "some_password";
 
 GRANT SELECT,INSERT,UPDATE,DELETE
-ON `account_db`.*
-TO account_user@localhost
+ON account_db.*
+TO account_user@localhost;
 
 GRANT SELECT,INSERT,UPDATE,DELETE
-ON `account_db`.*
+ON account_db.*
 TO account_user@'%'
--- finalTask/sql/3_create_tables.sql
+++ finalTask/sql/3_create_tables.sql
@@ -1,4 +1,4 @@
-USE `account_db`;
+USE account_db;
 
 CREATE TABLE user (
 	id INTEGER NOT NULL AUTO_INCREMENT,
@@ -8,35 +8,66 @@ CREATE TABLE user (
 	 * 0 - студент (Role.STUDENT)
 	 * 1 - староста (Role.LEADER)
 	 * 2 - работник деканата (Role.ADMINISTRATOR)
+	 * 3 - преподаватель (Role.TUTOR)
 	 */
-	role TINYINT NOT NULL CHECK (role IN (0, 1, 2)),
-	PRIMARY KEY (id)
-)
-
-CREATE TABLE student (
-	id INTEGER NOT NULL AUTO_INCREMENT, -- номер студенческого билета
+	role TINYINT NOT NULL CHECK (role IN (0, 1, 2, 3)),
 	surname VARCHAR(255) NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	patronymic VARCHAR(255) NOT NULL, -- отчество
-	faculty VARCHAR(255) NOT NULL,
-	course_number INTEGER NOT NULL,
+	CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+/*
+ * таблицы-справочники:
+ * - время начала пар, продолжительность пар и перерывов
+ * (для вычисления времени начала и конца пары)
+ * - диспилины
+ * - преподаватели
+ * - факультет
+ */
+
+ CREATE TABLE  pairtime (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    beginning_time CHAR(32) NOT NULL,
+    pair_duration CHAR(32) NOT NULL,
+    break_duration CHAR(32) NOT NULL,
+    CONSTRAINT pk_pairtime PRIMARY KEY (id)
+ );
+
+ CREATE TABLE subject (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_subject PRIMARY KEY (id)
+ );
+
+CREATE TABLE classroom (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    number INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
+    /*
+	 * 0 - семианрская (Type.SEMINAR)
+	 * 1 - лекционная (Type.LECTURE)
+	 */
+    type TINYINT NOT NULL CHECK (type IN (0, 1)),
+    hasProjector BIT NOT NULL,
+    CONSTRAINT pk_classroom PRIMARY KEY (id)
+);
+
+CREATE TABLE faculty (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    dean VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_faculty PRIMARY KEY (id)
+);
+
+/*
+ * конец таблиц-справочников
+ */
+
+ CREATE TABLE timetable (
+    course_number INTEGER NOT NULL,
 	group_number INTEGER NOT NULL,
-	isLeader BIT DEFAULT 0,
-	PRIMARY KEY (id)
-)
-
-CREATE TABLE administrator (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	surname VARCHAR(255) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	patronymic VARCHAR(255) NOT NULL,
-	position VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
-)
-
-CREATE TABLE timetable (
-	id INTEGER NOT NULL AUTO_INCREMENT, -- номер пары
-	/*
+    /*
 	 * 0 - пн (Day.MONDAY)
 	 * 1 - вт (Day.TUESDAY)
 	 * 2 - ср (Day.WEDNESDAY)
@@ -44,26 +75,59 @@ CREATE TABLE timetable (
 	 * 4 - пт (Day.FRIDAY)
 	 * 5 - сб (Day.SATURDAY)
 	 */
-	day TINYINT NOT NULL CHECK (role IN (0, 1, 2, 3, 4, 5)),
-	beginning_time CHAR(32) NOT NULL,
-	end_time CHAR(32) NOT NULL,
-	subject VARCHAR(255) NOT NULL,
+	day TINYINT NOT NULL CHECK (day IN (0, 1, 2, 3, 4, 5)),
+	pair_number INTEGER NOT NULL, -- номер пары
+	pairtime_id INTEGER NOT NULL,
+	subject_id INTEGER NOT NULL,
 	/*
-	 * 0 - лекция (Role.LECTURE)
-	 * 1 - практическое занятие (Role.PRACTICE)
-	 * 2 - семинар (Role.SEMINAR)
+	 * 0 - лекция (Type.LECTURE)
+	 * 1 - практическое занятие (Type.PRACTICE)
+	 * 2 - семинар (Type.SEMINAR)
 	 */
-	type TINYINT NOT NULL CHECK (role IN (0, 1, 2)),
-	lecturer VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (`author_identity`)
-	REFERENCES `authors` (`identity`)
-	ON UPDATE CASCADE
-	ON DELETE RESTRICT
-)
+	type TINYINT NOT NULL CHECK (type IN (0, 1, 2)),
+	classroom_id INTEGER NOT NULL,
+	tutor_id INTEGER NOT NULL,
+	CONSTRAINT pk_timetable
+	    PRIMARY KEY (course_number, group_number, day, pair_number),
+	CONSTRAINT fk_timetable_pairtime FOREIGN KEY (pairtime_id) REFERENCES pairtime (id),
+	CONSTRAINT fk_timetable_subject FOREIGN KEY (subject_id) REFERENCES subject (id),
+	CONSTRAINT fk_timetable_classroom FOREIGN KEY (classroom_id) REFERENCES classroom (id)
+);
 
-/*
- * таблицы-справочники:
- * - диспилины
- * - преподаватели
- */
+CREATE TABLE student (
+	id INTEGER NOT NULL, -- номер студенческого билета
+	faculty_id INTEGER NOT NULL,
+	student_course INTEGER NOT NULL,
+	student_group INTEGER NOT NULL,
+	student_subgroup BIT,
+	user_id INTEGER NOT NULL,
+	CONSTRAINT pk_student PRIMARY KEY (id),
+	CONSTRAINT fk_student_faculty FOREIGN KEY (faculty_id) REFERENCES faculty (id),
+	CONSTRAINT fk_student_timetable FOREIGN KEY (student_course, student_group)
+	    REFERENCES timetable (course_number, group_number),
+	CONSTRAINT fk_student_user FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE administrator (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	position VARCHAR(255) NOT NULL,
+	user_id INTEGER NOT NULL,
+	CONSTRAINT pk_administrator PRIMARY KEY (id),
+	CONSTRAINT fk_administrator_user FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE tutor (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    position VARCHAR(255) NOT NULL,
+    degree VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT pk_tutor PRIMARY KEY (id),
+	CONSTRAINT fk_tutor_user FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE leader (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    student_id INTEGER NOT NULL,
+    CONSTRAINT pk_leader PRIMARY KEY (id),
+    CONSTRAINT fk_leader_student FOREIGN KEY (student_id) REFERENCES student (id)
+);
